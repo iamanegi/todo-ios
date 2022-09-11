3cce1246d82ed0a02bb1391e0dd1d2bcc11b58df
aman <amannegi229@gmail.com>
2022-09-11 20:14:23 +0530

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Aman Negi on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

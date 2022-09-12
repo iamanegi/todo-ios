f0cdf395edd8c56dcda412ae01e32bcb24273696
aman <amannegi229@gmail.com>
2022-09-12 17:05:17 +0530

    handled UI of AddTask screen

--- todo/Views/AddTaskView.swift
+++ todo/Views/AddTaskView.swift
@@ -9,11 +9,59 @@ import SwiftUI
 
 struct AddTaskView: View {
     
+    // *1 - to dismiss a view
+    @Environment(\.presentationMode) var presentationMode
+    
     @EnvironmentObject var listViewModel: ListViewModel
     
+    @State var textFieldText: String = ""
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                TextField("Add new task...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(.gray.opacity(0.3))
+                    .cornerRadius(10)
+                
+                Button(action: saveMyTask) {
+                    Text("Save")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                }
+            }.padding()
+        }.navigationTitle("Add an Item")
+            .alert(isPresented: $showAlert, content: getAlert)
     }
+    
+    func saveMyTask() {
+        if textIsValid() {
+            listViewModel.addItem(Title: textFieldText)
+            // *2 - to dismiss a view
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsValid() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Task name is too small."
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
+    }
+    
 }
 
 struct AddTaskView_Previews: PreviewProvider {
